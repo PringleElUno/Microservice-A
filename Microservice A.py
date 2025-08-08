2da6ea2257ec8598ddc675f6f65cd95497ec6bd6
Noaboabit <89362288+Noahos@users.noreply.github.com>
2025-08-08 12:08:54 -0700

    Update Microservice A.py
    I implemented additional conditional logic in the while listening loop. Now, the microservice will trigger each of it's functions when receiving certain text strings from the main program via zeromq, receiving the string "import" triggers a bank retrieval by name, receiving the string "export" triggers saving a bank to storage. Doing this also required some additional request and response logic to be added, so a confirmation is returned after the first command is received, then the main program sends the desired name of the bank.
    
    I removed the blanket try/except block in the while listening loop, and I replaced it with more specific, localized try and except blocks. One try/except was added in the load storage method: when opening the file for read, if the file is corrupted, simply return an empty dictionary.
    
    The second try/except, is in the export logic: try saving to storage, if there's an error, send a failure message to the main program to preserve the REQ and RES handshake in zeromq, set current key to None, and move to the next loop iteration for continued listening.

--- Microservice A.py
+++ Microservice A.py
@@ -1,57 +1,80 @@
 import os
 import json
+from json import JSONDecodeError
+
 import zmq
 
-# Define the storage file, so that we can use the JSON 
+# Define the storage file, so that we can use the JSON
 STORAGE_FILE = "article_storage.json"
 
-# Loading or intilization of the storage for the articles
+
+# Loading or initialization of the storage for the articles
 def load_storage():
     if os.path.exists(STORAGE_FILE):
-        with open(STORAGE_FILE, 'r') as file: # Read functions allow for the user to open the data/message
-            return json.load(file)
+        try:
+            with open(STORAGE_FILE, 'r') as file:  # Read functions allow for the user to open the data/message
+                return json.load(file)
+        except json.JSONDecodeError:
+            print("JSON file is corrupted, clearing storage file")
+            return {}
     return {}
-    
+
+
 # A save storage function to call teh data
 def save_storage(data):
-    with open(STORAGE_FILE, 'w') as file: # Allows for the user to write the saved storage data/message
-        json.dump(data, file) 
+    with open(STORAGE_FILE, 'w') as file:  # Allows for the user to write the saved storage data/message
+        json.dump(data, file)
 
-# A set up to the ZEROMQ rep socket
+    # A set up to the ZEROMQ rep socket
+
+
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://*:7979") #Port of choosing is 7979
-print("Microservice is fully functional and live in the beautiful port of 7979...") # Print statement for when the microservice is connected to the port
+socket.bind("tcp://*:7979")  # Port of choosing is 7979
+print(
+    "Microservice is fully functional and live in the beautiful port of 7979...")  # Print statement for when the microservice is connected to the port
 
-# define the storage so it cna load a storage and call the whats inside the storage i.e the message
+# define the storage so it can load a storage and call the whats inside the storage i.e the message
 storage = load_storage()
 current_key = None
 
-
 while True:
-    try: # allows for crashs to be handled without crashing complete server and constant looping incase of incorrect json files
+
         message = socket.recv_string()
 
-        # This will check for the key if it is stored and to retrieve it
-        if message in storage:
-            print(f"Retrieving articles for: {message}")
-            print(f"Returned the articles: {json.dumps(storage[message])}")
-            socket.send_string(json.dumps(storage[message])) # ZeroMQ send string
-            continue
-        
-        # if the key is not set correctly then we want to treat the message as the same
-        if current_key is None:
-            current_key = message
-            socket.send_string("Got the Name!: Lets send that article list!")
-        # The second message with regards to the list of dictionaries
-        else:
-            article_list = json.loads(message)
-            storage[current_key] = article_list
-            save_storage(storage)
-            print(f"Stored the articles in the: {current_key}")
-            current_key = None
-            socket.send_string("The articles was stored!")
-    # Our exception error whenever the server reaches a error, the try statement embedded will aid the server to not crash completely!
-    except Exception as error:
-        socket.send_string(f"Error! {str(error)}")
-        current_key = None
+        if message == "import":
+            confirmation_message = "Ready to retrieve bank, send the name of the bank"
+            socket.send_string(confirmation_message)
+            message = socket.recv_string()
+            # This will check for the key if it is stored and to retrieve it
+            if message in storage:
+                print(f"Retrieving articles for: {message}")
+                print(f"Returned the articles: {json.dumps(storage[message])}")
+                socket.send_string(json.dumps(storage[message]))  # ZeroMQ send string
+                continue
+            else:
+                print("No bank corresponding to the name you sent.")
+                confirmation_message = "None"
+                socket.send_string(confirmation_message)
+                continue
+        if message == "export":
+            confirmation_message = "Export request received, send the bank name"
+            socket.send_string(confirmation_message)
+            message = socket.recv_string()
+            # if the key is not set correctly then we want to treat the message as the same
+            if current_key is None:
+                current_key = message
+                socket.send_string("Got the Name!: Lets send that article list!")
+            # The second message from the main program is the list of dictionaries (bank)
+                message = socket.recv_string()
+                article_list = json.loads(message)
+                storage[current_key] = article_list
+                try:
+                    save_storage(storage)
+                    print(f"Stored the articles for the key : {current_key}")
+                    current_key = None
+                    socket.send_string(f"The bank was stored!")
+                except Exception as error:
+                    print(f"Articles failed to save to storage for the key:  {current_key}")
+                    current_key = None
+                    socket.send_string(f"Articles failed to save to storage for the key:  {current_key}")
